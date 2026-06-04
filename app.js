c84e0367906fc4fd60fe8d90180d212d0926c9f3
Kere14 <akerejeta@hjgaray.com>
2026-06-04 10:52:08 +0200

    app.js

--- app.js
+++ app.js
@@ -4,9 +4,9 @@
    ============================================================ */
 
 const DATA_SRC = 'https://raw.githubusercontent.com/openfootball/worldcup.json/refs/heads/master/2026';
-const LEADERBOARD_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vSDwcurPFZ1PgxTQ_o_D4D4Xjboy1lUrU711uWdLIKXVnWofbf_CwGEeaTL0VaLAX7SOHlRYBCuybu_/pub?gid=1132102352&single=true&output=csv'
-const FORM_ID = '1FAIpQLSd9OPSO4JwC6aDS0dtN9FkpmIiCCijgQztklxLC410HTgvjUg';
-const ENTRY_ID = 'entry.479239932';
+const LEADERBOARD_CSV_URL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vR50so94MB_d_s9v97HJFE76w2CK6WVP3UsQNotTJHZOqQHg5FCUiX58anac0nb7bJtambpsOQp7X7P/pub?gid=501263411&single=true&output=csv'
+const FORM_ID = '1sl9v5Q7XJdk-FaWxqWwaBKhfs0kHMR5dTz0HAYeCCjY';
+const ENTRY_ID = 'entry.671695863';
 
 const puntuaciones = {
   grupos: {
@@ -1491,7 +1491,7 @@ const AWARD_PLAYERS = [
   { name: 'Azmahar Ariano', country: 'Panamá' },
 
   // 🐣 Easter Egg — Euskadi (País Vasco)
-  { name: 'Aritz Tokero Galdos', country: 'Euskadi (País Vasco)' },
+  { name: 'Kere', country: 'Euskadi (País Vasco)' },
   { name: 'Jon Escalador Dorronsoro', country: 'Euskadi (País Vasco)' }
 
 ];
